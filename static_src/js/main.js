078899e1de62bb7ad816343e69198d518ce6944d
s-kobets <s-kobets617@yandex.ru>
2017-03-06 00:41:46 +0300

    add Pug

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -1,6 +1,6 @@
 /* global */
-// const Vue = require('vue');
-// import createApp from './create-app';
-// console.log(33333333333333333, createApp(Vue));
+const Vue = require('vue');
+import createApp from './create-app';
+console.log(33333333333333333, createApp(Vue));
 
 require('../css/main.css');
