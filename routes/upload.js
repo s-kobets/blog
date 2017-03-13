a014772003fb8beea79cbf04eb2872b3f1ef0d99
s-kobets <s-kobets617@yandex.ru>
2017-03-13 08:19:47 +0300

    upload file

--- routes/upload.js
+++ routes/upload.js
@@ -2,27 +2,33 @@ import express from 'express';
 import multer from 'multer';
 
 const router = express.Router();
-const fs = require('fs-extra');
+// const fs = require('fs-extra');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    let path = '../public/uploads/';
-    fs.mkdirsSync(path);
-    cb(null, path);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname)
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     let path = './uploads/';
+//     fs.mkdirsSync(path);
+//     cb(null, path);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.fieldname)
+//   }
+// });
 
 const uploading = multer({
-  dest: __dirname + '/../public/uploads/',
+  dest: './uploads/',
   limits: {files:1},
-  storage: storage
+  fieldSize: 1000000,
+  inMemory: true
+  // ,
+  // storage: storage
 });
 
-router.post('/upload',uploading.single('image'), (req, res) => {
-  	res.status(200).send();
+//single('image')
+router.post('/upload', uploading.single('image'), (req, res) => {
+	res.json(req.file);
+	console.log('uploads', req.file);
+  	res.end();
 });
 
 export default router;
